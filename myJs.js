e710e759cae64308d2ad2872db1b86f06258a84f
takyuchan <frankychantakyu2@gmail.com>
2020-04-03 17:19:51 +1300

    Updated Navigation bar opacity to 1

--- myJs.js
+++ myJs.js
@@ -7,12 +7,14 @@ const spanMore = document.querySelector('.more');
 //Button Event Handlers
 let isMore = false;
 readMoreBtn.onclick = () => {
+    //Clicking readMoreBtn to expand the content
     if(!isMore) {
         spanDots.style.display = 'none';
         spanMore.style.display = 'inline';
         readMoreBtn.innerHTML = 'Read less';
         isMore = true;
     }
+    //Clicking readMoreBtn to close the content
     else {
         spanDots.style.display = 'inline';
         spanMore.style.display = 'none';
