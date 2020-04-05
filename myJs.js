71a4cf6ce7151c99eb2e43292b5da2c8a198282d
takyuchan <frankychantakyu2@gmail.com>
2020-04-05 20:29:46 +1200

    Updated text responsiveness and pop up navigation UI

--- myJs.js
+++ myJs.js
@@ -1,74 +1,73 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  //Dom element
+  const readMoreBtn = document.querySelector(".read-more");
 
-    //Dom element
-    const readMoreBtn = document.querySelector('.read-more');
+  const spanDots = document.querySelector(".dots");
+  const spanMore = document.querySelector(".more");
 
-    const spanDots = document.querySelector('.dots');
-    const spanMore = document.querySelector('.more');
+  const compass = document.querySelector(".icon-blue");
+  const back = document.querySelector(".icon-white");
+  const ui = document.querySelector(".menu");
+  const uiSearchBar = document.querySelector(".search-bar");
+  const data = document.querySelector(".data");
 
-    const compass = document.querySelector('.icon-blue');
-    const back = document.querySelector('.icon-white');
-    const ui = document.querySelector('.menu');
-    const uiSearchBar = document.querySelector('.search-bar');
-    const data = document.querySelector('.data');
+  let searchBarOpen = false;
 
-    let searchBarOpen = false
-
-    //Button Event Handlers
-    let isMore = false;
-    readMoreBtn.onclick = () => {
-        //Clicking readMoreBtn to expand the content
-        if(!isMore) {
-            spanDots.style.display = 'none';
-            spanMore.style.display = 'inline';
-            readMoreBtn.innerHTML = 'Read less';
-            isMore = true;
-        }
-        //Clicking readMoreBtn to close the content
-        else {
-            spanDots.style.display = 'inline';
-            spanMore.style.display = 'none';
-            readMoreBtn.innerHTML = 'Read more';
-            isMore = false;
-        }
+  //Button Event Handlers
+  let isMore = false;
+  readMoreBtn.onclick = () => {
+    //Clicking readMoreBtn to expand the content
+    if (!isMore) {
+      spanDots.style.display = "none";
+      spanMore.style.display = "inline";
+      readMoreBtn.innerHTML = "Read less";
+      isMore = true;
     }
-
-    //UI shows up when compass is clicked;
-    // let menuOpen = false;
-    compass.onclick = () => {
-        ui.style.opacity = '1';
-        ui.style.top = '0';
-        searchBarOpen = false;
+    //Clicking readMoreBtn to close the content
+    else {
+      spanDots.style.display = "inline";
+      spanMore.style.display = "none";
+      readMoreBtn.innerHTML = "Read more";
+      isMore = false;
     }
+  };
 
-    back.onclick = () => {
-        ui.style.opacity = '0';
-        ui.style.top = '-999px';
-        searchBarOpen = false;
+  //UI shows up when compass is clicked;
+  // let menuOpen = false;
+  compass.onclick = () => {
+    ui.style.opacity = "1";
+    ui.style.top = "0";
+    searchBarOpen = false;
+  };
+
+  back.onclick = () => {
+    ui.style.opacity = "0";
+    ui.style.top = "-999rem";
+    searchBarOpen = false;
+  };
+
+  //If SearchBarOpen is just clicked, user can click everywhere to close the searchBar but not
+  //the UI
+  $(".search-bar, .search-bar__submit, .search-bar__input").click(() => {
+    searchBarOpen = true;
+  });
+
+  $(data).click(() => {
+    searchBarOpen = false;
+  });
+
+  //Clicking on the area other than menu will go back to main page
+  $(ui).click(() => {
+    if (!searchBarOpen) {
+      ui.style.opacity = "0";
+      ui.style.top = "-999rem";
+    } else {
+      searchBarOpen = false;
     }
-
-    //If SearchBarOpen is just clicked, user can click everywhere to close the searchBar but not
-    //the UI
-    $('.search-bar, .search-bar__submit, .search-bar__input').click(() => {
-        searchBarOpen = true;
+  });
+  $(data)
+    .add(".search-bar, .search-bar__submit, .search-bar__input")
+    .click(function () {
+      event.stopPropagation();
     });
-
-    $(data).click(() => {
-        searchBarOpen = false;
-    });
-
-    //Clicking on the area other than menu will go back to main page
-    $(ui).click(() => {
-        if (!searchBarOpen) {
-            ui.style.opacity = '0';
-            ui.style.top = '-999px';
-        }
-        else {
-            searchBarOpen = false;
-        }
-    });
-    $(data).add('.search-bar, .search-bar__submit, .search-bar__input').click(function() {
-        event.stopPropagation();
-    });
-
 });
