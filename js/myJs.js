348bd5c1fcb70acfbb5b035a80dcb5057269ce6b
takyuchan <frankychantakyu2@gmail.com>
2020-04-21 01:07:25 +1200

    Venus required mobile friendly feature for floating nav

--- js/myJs.js
+++ js/myJs.js
@@ -1,21 +1,34 @@
-//Floating Nav: _nav.scss
-const aucklandSection = $(".section-content--auckland");
-const floatNav = $(".navigation");
+$(document).ready(() => {
+  //Floating Nav: _nav.scss
+  const aucklandSection = $(".section-content--auckland");
+  const floatNav = $(".navigation");
 
-const nav_checkbox = $(".navigation__checkbox");
+  const nav_checkbox = $(".navigation__checkbox");
 
-//Only apply waypoint to the floating navigation when the viewport min-width >= 800px
-const viewport_md = window.matchMedia("(min-width: 800px)");
-aucklandSection.waypoint(
-  (direction) => {
-    if (direction == "down") {
-      floatNav.addClass("show");
-    } else {
-      if (viewport_md.matches) {
-        floatNav.removeClass("show");
-        nav_checkbox.prop("checked", false);
+  //Only apply waypoint to the floating navigation when the viewport min-width >= 800px
+  const viewport_md = window.matchMedia("(min-width: 800px)");
+  aucklandSection.waypoint(
+    (direction) => {
+      if (direction == "down") {
+        floatNav.addClass("show");
+      } else {
+        if (viewport_md.matches) {
+          floatNav.removeClass("show");
+          nav_checkbox.prop("checked", false);
+        }
       }
-    }
-  },
-  { offset: "10%" }
-);
+    },
+    { offset: "10%" }
+  );
+
+  const nav = $(".navigation__nav");
+  // const nav_btn = $(".navigation__button");
+  const nav_items = $(".navigation__item");
+  $(nav).click(() => {
+    nav_checkbox.prop("checked", false);
+  });
+
+  $(nav_items).click(() => {
+    event.stopPropagation();
+  });
+});
