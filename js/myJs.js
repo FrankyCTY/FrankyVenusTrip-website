657cfae2a19648dc3581ca6addcce61f2c641374
takyuchan <frankychantakyu2@gmail.com>
2020-04-20 20:30:27 +1200

    Added waypoint and jquery

--- js/myJs.js
+++ js/myJs.js
@@ -1,11 +1,26 @@
-const nav = document.querySelector(".nav");
+const nav = $(".nav");
 const openNavBtn = document.querySelector(".btn-open-nav");
 const closeNavBtn = document.querySelector(".btn-close-nav");
 
 openNavBtn.addEventListener("click", () => {
-  nav.classList.add("opened-nav");
+  nav.addClass("opened-nav");
 });
 
 closeNavBtn.addEventListener("click", () => {
-  nav.classList.remove("opened-nav");
+  nav.removeClass("opened-nav");
 });
+
+//Floating Nav: _nav.scss
+const aucklandSection = $(".section-content--auckland");
+const floatNav = $(".navigation");
+
+aucklandSection.waypoint(
+  (direction) => {
+    if (direction == "down") {
+      floatNav.addClass("show");
+    } else {
+      floatNav.removeClass("show");
+    }
+  },
+  { offset: "-10%" }
+);
