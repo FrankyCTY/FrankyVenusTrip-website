f9f067754ee0e5440fab4ee60354e1cce5e4b794
takyuchan <frankychantakyu2@gmail.com>
2020-04-23 02:30:17 +1200

    Finished Home.html and added GSAP

---
+++ js/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+  const hero = $(".hero");
+  const slider = $(".slider");
+
+  const textbox = $(".text-box");
+
+  const logo = $(".logo-link");
+  const nav = $(".nav");
+
+  const tl = new TimelineMax();
+
+  // =================== Layout animation ==================
+
+  tl.fromTo(
+    hero,
+    1,
+    { height: "0%" },
+    { height: "60%", ease: Power2.easeInOut }
+  ).fromTo(
+    slider,
+    1,
+    { width: "0%" },
+    { width: "100%", ease: Power2.easeInOut }
+  );
+
+  // ================== Element fade In =========================
+
+  const tl2 = new TimelineMax();
+
+  tl2
+    .fromTo(
+      textbox,
+      1.5,
+      { opacity: 0 },
+      { opacity: 1, ease: Power2.easeInOut }
+    )
+    .fromTo(
+      nav,
+      1.5,
+      { opacity: 0, x: 30 },
+      { opacity: 1, x: 0, ease: Power2.easeInOut },
+      "-=1.5"
+    )
+    .fromTo(
+      logo,
+      1.5,
+      { autoAlpha: 0, x: 30 },
+      { autoAlpha: 1, x: 0, ease: Power2.easeInOut },
+      "-=1.5"
+    );
+});
